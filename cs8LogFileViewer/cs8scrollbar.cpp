b78fe20efdbb9d3cb484c7e85dd0b3b4520a750a
vdg1 <volker.drewer@gmail.com>
2018-09-01 19:27:43 +0200

    Added line indicator as tool tip on vertical scrollbar

--- cs8LogFileViewer/cs8scrollbar.cpp
+++ cs8LogFileViewer/cs8scrollbar.cpp
@@ -3,72 +3,76 @@
 #include <QDebug>
 #include <QPainter>
 #include <QStyleOptionSlider>
+#include <QToolTip>
 
-
-cs8ScrollBar::cs8ScrollBar(QWidget *parent):
-    QScrollBar(parent), m_rowCount(0), m_reversed(false)
-{
-
+cs8ScrollBar::cs8ScrollBar(QWidget *parent) : QScrollBar(parent), m_rowCount(0), m_reversed(false), m_model(0) {
+  setMouseTracking(true);
 }
 
-void cs8ScrollBar::addHighlight(int row, int totalRow, QBrush brush)
-{
-    highLights[row]=brush;
-    m_rowCount=totalRow;
+void cs8ScrollBar::setModel(QAbstractItemModel *model) { m_model = model; }
+
+void cs8ScrollBar::addHighlight(int row, int totalRow, QBrush brush) {
+  highLights[row] = brush;
+  m_rowCount = totalRow;
 }
 
-void cs8ScrollBar::resetHighlight()
-{
-    highLights.clear();
-    update();
+void cs8ScrollBar::resetHighlight() {
+  highLights.clear();
+  update();
 }
 
-void cs8ScrollBar::completedHighlight()
-{
-    update();
+void cs8ScrollBar::completedHighlight() { update(); }
+
+void cs8ScrollBar::reverseHighlights(bool reverse) {
+  m_reversed = reverse;
+  update();
 }
 
-void cs8ScrollBar::reverseHighlights(bool reverse)
-{
-    m_reversed=reverse;
-    update();
+void cs8ScrollBar::paintEvent(QPaintEvent *event) {
+  QScrollBar::paintEvent(event);
+
+  QPainter p(this);
+  QStyleOptionSlider opt;
+
+  initStyleOption(&opt);
+
+  QRect gr = style()->subControlRect(QStyle::CC_ScrollBar, &opt, QStyle::SC_ScrollBarGroove, this);
+  QRect sr = style()->subControlRect(QStyle::CC_ScrollBar, &opt, QStyle::SC_ScrollBarSlider, this);
+  sr.adjust(1, 0, -1, 0);
+  p.setClipRegion(QRegion(gr) - QRegion(sr), Qt::IntersectClip);
+  int x, y, w, h;
+  gr.getRect(&x, &y, &w, &h);
+  QColor c;
+  QMapIterator<int, QBrush> i(highLights);
+  int pos = 0;
+  auto posFract = 0.0;
+
+  while (i.hasNext()) {
+    i.next();
+    posFract = static_cast<double>(i.key()) / m_rowCount;
+    if (!m_reversed)
+      pos = static_cast<double>(posFract) * h;
+    else
+      pos = static_cast<double>((1 - posFract)) * h;
+    c = i.value().color();
+    c.setAlpha(200);
+
+    p.setPen(QPen(c, 1, Qt::SolidLine));
+    // p.drawRect(x,y,w,h-1);
+    p.drawLine(0, pos + y, w, pos + y);
+    // qDebug() << i.key() << ": " << i.value() << endl;
+  }
 }
 
-void cs8ScrollBar::paintEvent(QPaintEvent *event)
-{
-    QScrollBar::paintEvent(event);
-
-    QPainter p(this);
-    QStyleOptionSlider opt;
-
-    initStyleOption(&opt);
-
-    QRect gr = style()->subControlRect(QStyle::CC_ScrollBar, &opt,
-                                       QStyle::SC_ScrollBarGroove, this);
-    QRect sr = style()->subControlRect(QStyle::CC_ScrollBar, &opt,
-                                       QStyle::SC_ScrollBarSlider, this);
-    sr.adjust(1,0,-1,0);
-    p.setClipRegion(QRegion(gr) - QRegion(sr) ,  Qt::IntersectClip);
-    int x,y,w,h;
-    gr.getRect(&x, &y, &w, &h);
-    QColor c;
-    QMapIterator<int, QBrush> i(highLights);
-    int pos=0;
-    auto posFract=0.0;
-
-    while (i.hasNext()) {
-        i.next();
-        posFract=static_cast<double>(i.key())/m_rowCount;
-        if (!m_reversed)
-            pos=static_cast<double>(posFract)*h;
-        else
-            pos=static_cast<double>((1-posFract))*h;
-        c=i.value().color();
-        c.setAlpha(200);
-
-        p.setPen(QPen(c,1,Qt::SolidLine));
-        //p.drawRect(x,y,w,h-1);
-        p.drawLine(0,pos+y,w,pos+y);
-        //qDebug() << i.key() << ": " << i.value() << endl;
-    }
+void cs8ScrollBar::mouseMoveEvent(QMouseEvent *event) {
+  QScrollBar::mouseMoveEvent(event);
+  qDebug() << minimum() << maximum() << event->pos() << height();
+  if (m_model != Q_NULLPTR) {
+    double row = (event->pos().y() - 17);
+    row /= (height() - 17 * 2);
+    row = row < 0 ? 0 : row > 1 ? 1 : row;
+    row *= m_model->rowCount();
+    row = (int)row;
+    QToolTip::showText(mapToGlobal(event->pos()), QString("Row: %3").arg(row));
+  }
 }
--- cs8LogFileViewer/cs8scrollbar.h
+++ cs8LogFileViewer/cs8scrollbar.h
@@ -1,25 +1,30 @@
 #ifndef CS8SCROLLBAR_H
 #define CS8SCROLLBAR_H
 
-#include <QScrollBar>
 #include <QPaintEvent>
+#include <QScrollBar>
 
-class cs8ScrollBar : public QScrollBar
-{
-    Q_OBJECT
+class QToolTip;
+class QAbstractItemModel;
+
+class cs8ScrollBar : public QScrollBar {
+  Q_OBJECT
 public:
-    cs8ScrollBar ( QWidget * parent = Q_NULLPTR );
+  cs8ScrollBar(QWidget *parent = Q_NULLPTR);
+  void setModel(QAbstractItemModel *model);
 public slots:
-    void addHighlight(int row, int totalRow, QBrush brush);
-    void resetHighlight();
-    void completedHighlight();
-    void reverseHighlights(bool reverse);
+  void addHighlight(int row, int totalRow, QBrush brush);
+  void resetHighlight();
+  void completedHighlight();
+  void reverseHighlights(bool reverse);
 
 protected:
-    void paintEvent(QPaintEvent * event);
-    QMap<int,QBrush> highLights;
-    int m_rowCount;
-    bool m_reversed;
+  void paintEvent(QPaintEvent *event) override;
+  void mouseMoveEvent(QMouseEvent *event) override;
+  QMap<int, QBrush> highLights;
+  int m_rowCount;
+  bool m_reversed;
+  QAbstractItemModel *m_model;
 };
 
 #endif // CS8SCROLLBAR_H
