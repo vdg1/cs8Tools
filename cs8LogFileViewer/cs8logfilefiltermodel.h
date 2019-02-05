f9c82b8caf6b848bad7b464dd0233dcb36dfc356
vdg <vdg@saxe-group.com>
2019-02-05 22:51:06 +0100

    Working on filters for INFO, WARNING and ERROR level messages

--- cs8LogFileViewer/cs8logfilefiltermodel.h
+++ cs8LogFileViewer/cs8logfilefiltermodel.h
@@ -5,39 +5,46 @@
 
 #include <QSortFilterProxyModel>
 
-
-
-class cs8LogFileFilterModel : public QSortFilterProxyModel
-{
-    Q_OBJECT
+class cs8LogFileFilterModel : public QSortFilterProxyModel {
+  Q_OBJECT
 
 public:
-    cs8LogFileFilterModel(QObject* parent = 0);
-    //bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const;
+  explicit cs8LogFileFilterModel(QObject *parent = 0);
+  // bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const;
 
-    void setSourceModel(logFileModel *model);
-    void setHideUserMessages(bool hide);
-    void setHideSwapFileMessages(bool hide);
+  void setSourceModel(logFileModel *model);
+  void setHideUserMessages(bool hide);
+  void setHideSwapFileMessages(bool hide);
 
-    bool hideUserMessages() const;
+  bool hideUserMessages() const;
 
-    bool hideSwapFileMessages() const;
+  bool hideSwapFileMessages() const;
 
-    bool reverseOrder() const;
-    void setReverseOrder(bool reverseOrder);
+  bool reverseOrder() const;
+  void setReverseOrder(bool reverseOrder);
+
+  bool showInformation() const;
+  void setShowInformation(bool showInformation);
+
+  bool showWarning() const;
+  void setShowWarning(bool showWarning);
+
+  bool showError() const;
+  void setShowError(bool showError);
 
 signals:
-    bool orderChanged(bool reversed);
+  bool orderChanged(bool reversed);
 
 protected:
-    bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const Q_DECL_OVERRIDE;
-    bool lessThan(const QModelIndex &left, const QModelIndex &right) const Q_DECL_OVERRIDE;
+  bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const Q_DECL_OVERRIDE;
+  bool lessThan(const QModelIndex &left, const QModelIndex &right) const Q_DECL_OVERRIDE;
 
-    //bool lessThan(const QModelIndex &left, const QModelIndex &right) const Q_DECL_OVERRIDE;
+  // bool lessThan(const QModelIndex &left, const QModelIndex &right) const Q_DECL_OVERRIDE;
 
 private:
-    bool m_hideUserMessages, m_hideSwapFileMessages, m_reverseOrder;
-    logFileModel *m_model;
+  bool m_hideUserMessages, m_hideSwapFileMessages, m_reverseOrder;
+  bool m_showInformation, m_showWarning, m_showError;
+  logFileModel *m_model;
 };
 
 #endif // CS8LOGFILEFILTERMODEL_H
